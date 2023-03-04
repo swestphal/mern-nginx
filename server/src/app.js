dbf5432bc1c6e84bd85a9c6612ee295821ec08b6
Simone Westphal <simone.westphal.me@gmail.com>
2023-03-04 19:00:10 +0100

    add axios and cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 const PORT = 3001
 const app = express()
 
+const corsOption = {
+    origin: 'http://localhost:3000'
+}
+app.use(cors(corsOption))
 
 app.get('/test', (req, res) => {
     res.send("Hello from express")
