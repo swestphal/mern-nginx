8cf17700d00c1801d884f86110d4a4d20e8a69cc
Simone Westphal <simone.westphal.me@gmail.com>
2023-03-05 11:44:15 +0100

    add mongo db to container

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,25 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
+const MONGO_PORT = process.env.MONGO_PORT || 27017
+const MONGO_URL = process.env.MONGO_URL || 'localhost'
+const MONGODB = `mongodb://${MONGO_URL}:${MONGO_PORT}`
+
+mongoose.connect(MONGODB).then(() => {
+    server.listen(port)
+    server.on('error', onError)
+    server.on('listening', onListening)
+    console.log(`mongodb connected ${MONGODB_URL}`)
+}).catch(err => {
+    console.log(err)
+    process.exit(1)
+})
+
 const PORT = process.env.PORT || 3001
 const CORS_PORT = process.env.CORS_PORT || 3000
 const CORS_URL = process.env.CORS_URL || 'localhost'
-console.log(PORT, CORS_PORT, CORS_URL)
+
 const app = express()
 
 const corsOption = {
